1cb70ffd546638d5c2f78f2942be5e201a944e1a
gondhes <fajarwirazdi@gmail.com>
2021-03-21 16:12:29 +0700

    fetch all product berhasil

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,69 +1,22 @@
 <template>
-  <div class="product">
-    <section class="product">
-      <div class="container">
-        <div class="row">
-          <!-- Product Card -->
-          <div class="col-3">
-            <div class="card">
-              <img class="card-img" src="https://cdn.shopify.com/s/files/1/0231/8024/7118/products/ME-003422_Transition_Jacket_ME-01488_BrackenMagma_8ce676b9-1dcd-4bd9-985a-64d5bab6a9df_438x648_crop_center.png?v=1600684143">
-              <div class="card-body">
-                <h5 class="card-name">Jacket</h5>
-                <h5 class="card-price">Rp350.000</h5>
-                <h5 class="card-stock">stock: 12</h5>
-                <a href="#" class="btn btn-edit">edit</a>
-                <a href="#" class="btn btn-delete">delete</a>
-              </div>
-            </div>
-          </div>
-          <!-- Product Card -->
-          <div class="col-3">
-            <div class="card">
-              <img class="card-img" src="https://cdn.shopify.com/s/files/1/0231/8024/7118/products/ME-003422_Transition_Jacket_ME-01488_BrackenMagma_8ce676b9-1dcd-4bd9-985a-64d5bab6a9df_438x648_crop_center.png?v=1600684143">
-              <div class="card-body">
-                <h5 class="card-name">Jacket</h5>
-                <h5 class="card-price">Rp350.000</h5>
-                <h5 class="card-stock">stock: 12</h5>
-                <a href="#" class="btn btn-edit">edit</a>
-                <a href="#" class="btn btn-delete">delete</a>
-              </div>
-            </div>
-          </div>
-          <!-- Product Card -->
-          <div class="col-3">
-            <div class="card">
-              <img class="card-img" src="https://cdn.shopify.com/s/files/1/0231/8024/7118/products/ME-003422_Transition_Jacket_ME-01488_BrackenMagma_8ce676b9-1dcd-4bd9-985a-64d5bab6a9df_438x648_crop_center.png?v=1600684143">
-              <div class="card-body">
-                <h5 class="card-name">Jacket</h5>
-                <h5 class="card-price">Rp350.000</h5>
-                <h5 class="card-stock">stock: 12</h5>
-                <a href="#" class="btn btn-edit">edit</a>
-                <a href="#" class="btn btn-delete">delete</a>
-              </div>
-            </div>
-          </div>
-          <!-- Product Card -->
-          <div class="col-3">
-            <div class="card">
-              <img class="card-img" src="https://cdn.shopify.com/s/files/1/0231/8024/7118/products/ME-003422_Transition_Jacket_ME-01488_BrackenMagma_8ce676b9-1dcd-4bd9-985a-64d5bab6a9df_438x648_crop_center.png?v=1600684143">
-              <div class="card-body">
-                <h5 class="card-name">Jacket</h5>
-                <h5 class="card-price">Rp350.000</h5>
-                <h5 class="card-stock">stock: 12</h5>
-                <a href="#" class="btn btn-edit">edit</a>
-                <a href="#" class="btn btn-delete">delete</a>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="col-3">
+    <div class="card">
+      <img class="card-img" :src="product.img_url">
+      <div class="card-body">
+        <h5 class="card-name">{{ product.name }}</h5>
+        <h5 class="card-price">Rp {{ product.price }}</h5>
+        <h5 class="card-stock">stock: {{ product.stock }}</h5>
+        <a href="#" class="btn btn-edit">edit</a>
+        <a href="#" class="btn btn-delete">delete</a>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProductCard'
+  name: 'ProductCard',
+  props: ['product']
 }
 </script>
 
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,16 +2,48 @@
   <div class="products">
     <h1>Our Products</h1>
     <br>
-    <ProductCard/>
+    <section class="product">
+      <div class="container">
+        <div class="row">
+          <!-- Product Card -->
+          <ProductCard
+            v-for="product in products"
+            :key="product.id"
+            :product="product"
+          />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import axios from '@/axios/axios.js'
 import ProductCard from '@/components/ProductCard.vue'
+
 export default {
   name: 'Products',
+  data () {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    fetchProducts () {
+      axios.get('/products')
+        .then(({ data }) => {
+          this.products = data
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
   components: {
     ProductCard
+  },
+  created () {
+    this.fetchProducts()
   }
 }
 </script>
